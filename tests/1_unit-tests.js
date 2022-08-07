945c564117a450f4393146e58b00069a205a27c8
diegomedina62 <diegomedina62@gmail.com>
2022-08-06 21:26:09 -0300

    functional test

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -16,7 +16,7 @@ const validPuzzle =
   "..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..";
 const coorObject = locator("a1");
 const objAnalysis = checker(coorObject, validPuzzle);
-console.log(objAnalysis);
+
 suite("Unit Tests", () => {
   test("81 characters", (done) => {
     assert.equal(solver.validate(validPuzzle), undefined);
